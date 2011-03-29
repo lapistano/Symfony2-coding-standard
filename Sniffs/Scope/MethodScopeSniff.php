2e37f31b4860db4d26a6fe4a1652f21f2c192037
Pascal Borreli <pascal@borreli.com>
2011-03-29 22:08:34 +0000

    [MethodScope] fixed > scope modifiers are not required for interface

--- Sniffs/Scope/MethodScopeSniff.php
+++ Sniffs/Scope/MethodScopeSniff.php
@@ -30,7 +30,7 @@ if (class_exists('PHP_CodeSniffer_Standards_AbstractScopeSniff', true) === false
  * @version   Release: 1.3.0
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
-class Symfony2_Sniffs_Scope_MethodScopeSniff extends Squiz_Sniffs_Scope_MethodScopeSniff
+class Symfony2_Sniffs_Scope_MethodScopeSniff extends PHP_CodeSniffer_Standards_AbstractScopeSniff
 {
     /**
      * Constructs a Symfony2_Sniffs_Scope_MethodScopeSniff.
@@ -41,4 +41,31 @@ class Symfony2_Sniffs_Scope_MethodScopeSniff extends Squiz_Sniffs_Scope_MethodSc
 
     }//end __construct()
 
+    /**
+     * Processes the function tokens within the class.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file where this token was found.
+     * @param int                  $stackPtr  The position where the token was found.
+     * @param int                  $currScope The current scope opener token.
+     *
+     * @return void
+     */
+    protected function processTokenWithinScope(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $currScope)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $methodName = $phpcsFile->getDeclarationName($stackPtr);
+        if ($methodName === null) {
+            // Ignore closures.
+            return;
+        }
+
+        $modifier = $phpcsFile->findPrevious(PHP_CodeSniffer_Tokens::$scopeModifiers, $stackPtr);
+        if (($modifier === false) || ($tokens[$modifier]['line'] !== $tokens[$stackPtr]['line'])) {
+            $error = 'No scope modifier specified for function "%s"';
+            $data  = array($methodName);
+            $phpcsFile->addError($error, $stackPtr, 'Missing', $data);
+        }
+
+    }//end processTokenWithinScope()
 }//end class
